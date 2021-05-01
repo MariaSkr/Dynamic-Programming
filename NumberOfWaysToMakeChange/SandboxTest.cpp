a32604386ab0a505c2c7819ae6d4f9e232029a6e
MariaSkr <maria.skriaghina@gmail.com>
2021-05-01 23:35:57 +0200

    Create SandboxTest.cpp

---
+++ NumberOfWaysToMakeChange/SandboxTest.cpp
@@ -0,0 +1,12 @@
+// This file is initialized with a code version of this
+// question's sample test case. Feel free to add, edit,
+// or remove test cases in this file as you see fit!
+
+class ProgramTest : public TestSuite {
+public:
+  void Run() {
+    RunTest("Test Case 1", []() {
+      assert(numberOfWaysToMakeChange(6, {1, 5}) == 2);
+    });
+  }
+};
