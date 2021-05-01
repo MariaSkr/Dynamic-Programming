d9f90b4728cc594c610411fbb6868f1dc0b78bbe
MariaSkr <maria.skriaghina@gmail.com>
2021-05-01 23:44:47 +0200

    Create SandboxTest.cpp

---
+++ LevenshteinDistance/SandboxTest.cpp
@@ -0,0 +1,11 @@
+// This file is initialized with a code version of this
+// question's sample test case. Feel free to add, edit,
+// or remove test cases in this file as you see fit!
+
+class ProgramTest : public TestSuite {
+public:
+  void Run() {
+    RunTest("Test Case 1",
+            []() { assert(levenshteinDistance("abc", "yabd") == 2); });
+  }
+};
